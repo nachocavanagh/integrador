f5eaac4b97dd96d28c31a6c97bcac6d5b8fb8f45
Bautista Lanussegit <blanusse@udesa.edu.ar>
2021-03-31 10:12:33 -0300

    Update index.js

--- proyectoInt/routes/index.js
+++ proyectoInt/routes/index.js
@@ -4,5 +4,7 @@ let indexController = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', indexController.index);
+router.get('/', indexController.index);
+
 
 module.exports = router;
